1e3a1a9b0af751060f5af15b0e7dd6cb48f50a7f
zeqxc <43218868+zeqxc@users.noreply.github.com>
2020-09-06 12:57:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="WrongUtils", # Replace with your own username
     version="0.0.1",
-    author="Example Author",
+    author="autixts",
     author_email="author@example.com",
-    description="A small example package",
+    description="A package to make life easier with discord.py",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
