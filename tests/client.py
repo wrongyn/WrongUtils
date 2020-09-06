e4364e0a4842b2bf9783ccf72a4efc4d6dfbd8e4
zeqxc <43218868+zeqxc@users.noreply.github.com>
2020-09-06 13:37:47 -0400

    Update client.py

--- tests/client.py
+++ tests/client.py
@@ -11,8 +11,8 @@ async def on_ready():
 @client.command()
 async def paginate(ctx):
   embed1 = discord.Embed(title="Page 1", description="This is page 1", color=0x00ff00)
-  embed1 = discord.Embed(title="Page 2", description="This is page 2", color=0x00ff00)
-  embed1 = discord.Embed(title="Page 3", description="This is page 3", color=0x00ff00)
+  embed2 = discord.Embed(title="Page 2", description="This is page 2", color=0x00ff00)
+  embed3 = discord.Embed(title="Page 3", description="This is page 3", color=0x00ff00)
   paginator = EmbedPaginator(ctx)
   embeds = [embed1, embed2, embed3]
   await paginator.run(embeds)
